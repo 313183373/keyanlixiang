2d9703a380aa90ee96fd312af0de25cc7a3beb1e
313183373 <313183373@qq.com>
2018-03-14 12:09:28 +0800

    add save function

--- demo.js
+++ demo.js
@@ -48,26 +48,18 @@ function getAllData() {
         (async () => {
 
             let itemsIHave = await Item.find({});
-            itemsIHave = itemsIHave.map(value => [value.name]);
-            console.log(itemsIHave);
+            itemsIHave = itemsIHave.map(value => value.name);
             console.log('Items I have: ' + itemsIHave.length);
 
-            // 导出已有的项目到excel中
-            // let buffer = xlsx.build([{
-            //     name:'sheet1',
-            //     data:itemsIHave
-            // }]);
-            // fs.writeFileSync('test1.xlsx',buffer,{'flag':'w'});
-
             // 获取重复词条数目
-            // let set = new Set(itemsIHave);
-            // console.log('repeat items I have: ' + Number(itemsIHave.length - set.size));
+            let set = new Set(itemsIHave);
+            console.log('repeat items I have: ' + Number(itemsIHave.length - set.size));
 
             // 滤除重复词条
-            // console.log('All items: ' + items.length);
-            // items = items.map(value => JSON.parse(value))
-            //     .filter(value => !itemsIHave.includes(value.name));
-            // console.log('Filter items:' + items.length);
+            console.log('All items: ' + items.length);
+            items = items.map(value => JSON.parse(value))
+                .filter(value => !itemsIHave.includes(value.name));
+            console.log('Filter items: ' + items.length);
 
 
             // 测试是否真的重复（发现是真的重复了，但是不知道为什么会出现重复词条）
@@ -83,38 +75,55 @@ function getAllData() {
             // })
 
             // 爬取
-            // async.mapLimit(items, 10, async function (item) {
-            //     let res = await request.get('http://www.baidu.com/s')
-            //         .query({wd: item.name})
-            //         .catch(function (err) {
-            //             if (err) console.error('request Error :' + err);
-            //         });
-            //     if (res && res.text) {
-            //         n++;
-            //         let $ = cheerio.load(res.text);
-            //         let cnt = $('.head_nums_cont_inner .nums').text().replace(/[^0-9]/g, '');
-            //         let i = new Item({
-            //             name: item.name,
-            //             number: cnt,
-            //             baikeUrl: undefined
-            //         });
-            //         i.save(function (err) {
-            //             if (err) {
-            //                 console.error(`${item.name} 存储出错!`);
-            //                 console.error(err);
-            //             } else {
-            //                 console.log(`${n}:${item.name}的结果数为:${cnt}`);
-            //             }
-            //         });
-            //         return cnt;
-            //     } else {
-            //         console.log(`${item.name}没有获取到!`);
-            //         lostItems.push(item);
-            //     }
-            // }, (err, results) => {
-            //     if (err) throw err;
-            //     console.log(results);
-            // });
+            let yibuCnt = 0;
+            async.mapLimit(items, 10, async function (item) {
+                yibuCnt ++;
+                let res = await request.get('http://www.baidu.com/s')
+                    .query({wd: item.name})
+                    .catch(function (err) {
+                        if (err) console.error('request Error :' + err);
+                    });
+                if (res && res.text) {
+                    n++;
+                    let $ = cheerio.load(res.text);
+                    let cnt = $('.head_nums_cont_inner .nums').text().replace(/[^0-9]/g, '');
+                    let i = new Item({
+                        name: item.name,
+                        number: cnt,
+                        baikeUrl: undefined
+                    });
+                    i.save(function (err) {
+                        if (err) {
+                            console.error(`${item.name} 存储出错!`);
+                            console.error(err);
+                        } else {
+                            console.log(`${n}:${item.name}的结果数为:${cnt},并发数为${yibuCnt}`);
+                        }
+                        yibuCnt --;
+                    });
+                    return cnt;
+                } else {
+                    console.log(`${item.name}没有获取到!`);
+                    lostItems.push(item);
+                    yibuCnt --;
+                }
+            }, (err, results) => {
+                if (err) throw err;
+                console.log(results);
+            });
         })();
     });
+}
+
+function saveDataToExcel() {
+    (async()=>{
+        let itemsIHave = await Item.find({});
+        itemsIHave = itemsIHave.map(value => value.name);
+        // 导出已有的项目到excel中
+        let buffer = xlsx.build([{
+            name:'sheet1',
+            data:itemsIHave
+        }]);
+        fs.writeFileSync('output.xlsx',buffer,{'flag':'w'});
+    })();
 }
